873b288f945cd3209649158cc55a7635448f3e7f
satyam kanaujiya <satyamkanaujiya02@gmail.com>
2025-02-27 20:39:12 +0530

    increase size of request body

--- server/app.js
+++ server/app.js
@@ -28,7 +28,7 @@ app.use(cors({
 }));
 app.use(helmet());
 app.use("/api",limiter);
-app.use(express.json({limit:"16kb"}));
+app.use(express.json({limit:"100mb"}));
 app.use(sanitize());
 app.use(express.urlencoded({extended:true,limit:"16kb"}));
 app.use(cookieParser());
